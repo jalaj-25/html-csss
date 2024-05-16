6e44778cef796868923c1d90a5d818c6ccd51e38
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2024-05-16 22:11:07 +0530

    Add files via upload

--- sundowm/sundowm/script.js
+++ sundowm/sundowm/script.js
@@ -1,3 +1,17 @@
-var a = document.querySelector("#elem1");
-var abc = a.getAttribute("class")
-console.log(abc)
+var fixed = document.querySelector("#fixed-image")
+var elemC = document.querySelector("#elem-container")
+elemC.addEventListener("mouseenter", function() {
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function() {
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+console.log(elems)
+elems.forEach(function(e) {
+    e.addEventListener("mouseenter", function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
